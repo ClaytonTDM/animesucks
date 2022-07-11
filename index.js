a586aab71ffbf183af8022d22cfc2bb6827af841
ClaytonTDM <claytontdm@gmail.com>
2022-07-11 19:54:14 -0400

    Fixed some things, added a new title

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var audio = new Audio('rickroll.mp3');
 function page1() {
 console.log('page1');
+console.log(8);
 document.oncontextmenu = function() {return false;}
 var countdown = 8;
 setInterval(function() {
@@ -12,11 +12,10 @@ setInterval(function() {
         document.getElementById('gone2').classList.remove('gone');
     }
 }, 1000);
-if (doConsoleUpdate == 1) {
 setInterval(function() {
-    console.clear();
+    if (countdown > 0) {
     console.log(countdown);
-}}
+    }}
 , 1000);
 setTimeout(function(){document.getElementById('gone').classList.remove('gone');},1000);
 setTimeout(function(){document.title = 'BOOM!';},800);
@@ -25,7 +24,11 @@ setTimeout(function(){document.getElementById('gone').classList.add('gone');},22
 setTimeout(function(){document.title = 'anime is gone :)';},2000);
 function rickroll() {
 setInterval(function() {
+    if (countdown > 0) {
     document.title = countdown;
+    } else {
+    document.title = 'click it';
+    }
     if(countdown == 1) {
     document.getElementById("countdown").innerHTML = 'Something cool will happen in ' + countdown + ' second...';
     } else {
@@ -36,18 +39,15 @@ setInterval(function() {
     }
     if(countdown == 'heheheha') {
         document.getElementById("countdown").innerHTML = 'get rickrolled idiot';
-    }
-}
-, 100);
-}
-setTimeout(function(){rickroll();},3500);
-}
+    }}
+, 10);}
+setTimeout(function(){rickroll();},3500);}
 function page2() {
+    adrielHacksOnALegoGame();
     function adrielHacksOnALegoGame() {
         const adrielHacksOnALegoGame = 1
         if(adrielHacksOnALegoGame = 1) {
         console.warn('hacking is cringe');
         } else {
         console.error('404 not found');
-        }}
-}
+        }}}
